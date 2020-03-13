7c8d534aaf1b1ceeea242420b6c0551b5a58daba
minhnx <minh.nx@isofh.com>
2020-03-13 21:31:39 +0700

    comit

---
+++ src/app/shared/model/comment.ts
@@ -0,0 +1,10 @@
+export class Comment {
+  id: Object;
+  // user:String;
+  recipe: Object;
+  content: String;
+  imageUrl: String;
+  videoUrl: String;
+  type: Number;
+  description: String = "Tính lượt thực hiện công thức";
+}
